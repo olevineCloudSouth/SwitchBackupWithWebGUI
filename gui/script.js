30d03bdc15c8386cf2eeb74bb2990550c5f77711
olevineCloudSouth <153957624+olevineCloudSouth@users.noreply.github.com>
2024-05-16 16:01:12 -0400

    working on gui

--- gui/script.js
+++ gui/script.js
@@ -1,11 +1,12 @@
-function getConfig() {
+function getConfigs() {
     const switchName = document.getElementById('switch').value;
-    const date = document.getElementById('date').value;
+    const new_date = document.getElementById('date1').value; 
+    const old_date = document.getElementById('date2').value;
     
     // Construct URL with query parameters
-    const url = `http://127.0.0.1:5000/get_config?switch_name=${encodeURIComponent(switchName)}&date=${encodeURIComponent(date)}`;
+    const new_url = `http://127.0.0.1:5000/get_config?switch_name=${encodeURIComponent(switchName)}&date=${encodeURIComponent(new_date)}`;
     
-    fetch(url, {
+    fetch(new_url, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
@@ -13,7 +14,21 @@ function getConfig() {
     })
     .then(response => response.text())
     .then(config => {
-        document.getElementById('configDisplay').textContent = config;
+        document.getElementById('configDisplay1').textContent = config;
+    })
+    .catch(error => console.error('Error fetching config:', error));
+
+    const old_url = `http://127.0.0.1:5000/get_config?switch_name=${encodeURIComponent(switchName)}&date=${encodeURIComponent(old_date)}`;
+    
+    fetch(old_url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.text())
+    .then(config => {
+        document.getElementById('configDisplay2').textContent = config;
     })
     .catch(error => console.error('Error fetching config:', error));
 }

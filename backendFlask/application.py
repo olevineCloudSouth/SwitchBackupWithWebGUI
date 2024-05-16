b9f92b0249b034900f8a3e2aeedcf282eb35c460
olevineCloudSouth <153957624+olevineCloudSouth@users.noreply.github.com>
2024-05-16 11:19:34 -0400

    creating framework for flask backend

---
+++ backendFlask/application.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+from endpoints.check_config import web_switch_check
+
+app = Flask(__name__)
+
+app.register_blueprint(web_switch_check)
+
+if __name__ == '__main__':
+    app.run()

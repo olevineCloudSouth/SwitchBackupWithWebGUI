9b569c386ab8ba382c1f12b017552ee79c582d38
olevineCloudSouth <153957624+olevineCloudSouth@users.noreply.github.com>
2024-05-28 16:16:56 -0400

    added ability for current config checks as well

--- backendFlask/flask_endpoints/check_config.py
+++ backendFlask/flask_endpoints/check_config.py
@@ -1,6 +1,14 @@
 from flask import Blueprint, jsonify, request
 from flask_cors import cross_origin
 import difflib
+import paramiko
+import time
+import pandas as pd
+import configparser
+
+from backendFlask.flask_endpoints.get_config import get_curr_config
+
+passwords = []
 
 def format_diff(diff_lines):
     formatted_diff = []
@@ -49,13 +57,25 @@ def check_changes(formatted_diff):
 
     return sections
 
-def compare_configs(config_file_today, config_file_yesterday):
-    try:
-        with open(config_file_today, 'r') as f_today, open(config_file_yesterday, 'r') as f_yesterday:
-            today_lines = f_today.readlines()[50:]  # Skip the first 20 lines
-            yesterday_lines = f_yesterday.readlines()[50:]  # Skip the first 20 lines
-    except FileNotFoundError:
-        return None, 10
+def compare_configs(config_file_today, config_file_yesterday, switch_name):
+    if config_file_today != 'current':
+        try:
+            with open(config_file_today, 'r') as f_today, open(config_file_yesterday, 'r') as f_yesterday:
+                today_lines = f_today.readlines()[50:]  # Skip the first 50 lines
+                yesterday_lines = f_yesterday.readlines()[50:]  # Skip the first 50 lines
+        except FileNotFoundError:
+            return None, 10
+    else:
+        try:
+            with open(config_file_yesterday, 'r') as f_yesterday:
+                    yesterday_lines = f_yesterday.readlines()[50:]  # Skip the first 50 lines
+        except FileNotFoundError:
+            return None, 10
+
+        
+        f_today = get_curr_config(switch_name, get_info())
+        today_lines = f_today.splitlines()[50:]
+
 
     diff = difflib.unified_diff(yesterday_lines, today_lines, lineterm='')
     diff_lines = list(diff)
@@ -68,18 +88,79 @@ def compare_configs(config_file_today, config_file_yesterday):
 
     return formatted_diff, 12
 
+def switch_touch(ip, password):
+    try:
+
+         # Create an SSH client instance
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_client.connect(hostname=ip, username='netadmin', password=password, timeout=5)
+        ssh_shell = ssh_client.invoke_shell()
+        ssh_shell.send("terminal length 0\n")  # Set terminal length to 0 to get the entire output
+        #wait until the shell is ready
+        while not ssh_shell.recv_ready():
+            pass
+        #write mem to save the config
+        ssh_shell.send("write mem\n")
+        #print out config
+        ssh_shell.send("show run\n")
+        #wait for it to finish
+        time.sleep(3)
+        #load config into string to pass
+        switch_output = ssh_shell.recv(65535).decode('utf-8')
+        i = 0
+        while not switch_output.endswith('#') or len(switch_output) < 1000:
+            time.sleep(1)
+            switch_output += ssh_shell.recv(65535).decode('utf-8')
+            i += 1
+            if i > 35:
+                return "Error: Connection timed out or switch took too long to respond", 14
+
+        switch_output = switch_output.strip()
+        return switch_output, 10
+    except Exception as e:
+        #traceback.print_exc()
+        print("Error: " + str(e) + "\n")
+        error = "Error: " + str(e) + "\n"
+        error = error.strip()
+        return error, 12
+    finally:
+         # Close SSH connection properly
+            ssh_client.close()
+
+def get_info():
+    df = pd.read_csv("/opt/backup-script/switch_ips.csv")
+    return df
+
+def get_curr_config (switch_name, switch_info):
+    switch_ip = switch_info.loc[switch_info['name'] == switch_name, 'switch_ips']
+    for pwd in passwords:
+        config, status_code = switch_touch(switch_ip, pwd)
+        if status_code == 10:
+            break
+    return config
+
 web_switch_check = Blueprint('web_switch_check', __name__)
 @web_switch_check.route('/switch_check', methods=['GET'])
 @cross_origin()
 def switch_check_main():
+    config = configparser.ConfigParser()
+    config.read('/opt/backup-script/pwds.ini')
+
+    for key in config['pwds']:
+        passwords.append(config['pwds'][key])
+
     new_date = request.args.get('new_date')
     old_date = request.args.get('old_date')
+
     check_switch = request.args.get('switch_name')
     if check_switch == None or new_date == None or old_date == None:
         return jsonify("Error missing params"), 400
     past_config = "/mnt/sda/switch-configs/{}/{}_config-{}.txt".format(old_date, check_switch, old_date)
     curr_config = "/mnt/sda/switch-configs/{}/{}_config-{}.txt".format(new_date, check_switch, new_date)
-    formatted_diff, status = compare_configs(curr_config, past_config)
+    if new_date == 'current': 
+        curr_config = 'current'
+    formatted_diff, status = compare_configs(curr_config, past_config, check_switch)
     if status == 12 and formatted_diff != None:
         #case where there are differences
         return jsonify(formatted_diff), 200
--- backendFlask/flask_endpoints/get_config.py
+++ backendFlask/flask_endpoints/get_config.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, jsonify, request
 from flask_cors import cross_origin
+import paramiko
+import time
+import pandas as pd
+import configparser
 
-
+passwords = []
 def get_config(file_path):
     try:
         with open(file_path, 'r') as file:
@@ -14,15 +18,75 @@ def get_config(file_path):
         print(f"Error reading file: {e}")
         return None
 
+def switch_touch(ip, password):
+    try:
+
+         # Create an SSH client instance
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_client.connect(hostname=ip, username='netadmin', password=password, timeout=5)
+        ssh_shell = ssh_client.invoke_shell()
+        ssh_shell.send("terminal length 0\n")  # Set terminal length to 0 to get the entire output
+        #wait until the shell is ready
+        while not ssh_shell.recv_ready():
+            pass
+        #write mem to save the config
+        ssh_shell.send("write mem\n")
+        #print out config
+        ssh_shell.send("show run\n")
+        #wait for it to finish
+        time.sleep(3)
+        #load config into string to pass
+        switch_output = ssh_shell.recv(65535).decode('utf-8')
+        i = 0
+        while not switch_output.endswith('#') or len(switch_output) < 1000:
+            time.sleep(1)
+            switch_output += ssh_shell.recv(65535).decode('utf-8')
+            i += 1
+            if i > 35:
+                return "Error: Connection timed out or switch took too long to respond", 14
+
+        switch_output = switch_output.strip()
+        return switch_output, 10
+    except Exception as e:
+        #traceback.print_exc()
+        print("Error: " + str(e) + "\n")
+        error = "Error: " + str(e) + "\n"
+        error = error.strip()
+        return error, 12
+    finally:
+         # Close SSH connection properly
+            ssh_client.close()
+
+def get_info():
+    df = pd.read_csv("/opt/backup-script/switch_ips.csv")
+    return df
+
+def get_curr_config (switch_name, switch_info):
+    switch_ip = switch_info.loc[switch_info['name'] == switch_name, 'switch_ips']
+    for pwd in passwords:
+        config, status_code = switch_touch(switch_ip, pwd)
+        if status_code == 10:
+            break
+    return config
 
 
 web_get_config = Blueprint('web_get_config', __name__)
 @web_get_config.route('/get_config', methods=['GET'])
 @cross_origin()
 def config_check_main():
+    config = configparser.ConfigParser()
+    config.read('/opt/backup-script/pwds.ini')
+
+    for key in config['pwds']:
+        passwords.append(config['pwds'][key])
     date = request.args.get('date')
     check_switch = request.args.get('switch_name')
     if check_switch == None or date == None:
         return jsonify("Error missing params"), 400
+    if check_switch == 'current':
+        switch_info = get_info()
+        return jsonify(get_curr_config(check_switch, switch_info)), 200
+            
     config_path = "/mnt/sda/switch-configs/{}/{}_config-{}.txt".format(date, check_switch, date)
     return jsonify(get_config(config_path)), 200
